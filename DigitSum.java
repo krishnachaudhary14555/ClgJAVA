33eb2c193362a2aab156d403deb74bd5d613529d
krishnachaudhary14555 <krishnachaudhary3942@gmail.com>
2026-08-23 00:34:49 +0530

    Sum of digits

---
+++ DigitSum.java
@@ -0,0 +1,19 @@
+
+// WAP to find the sum of digit of a number
+import java.util.*;
+public class DigitSum {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int org = n;
+        int sum =0;
+        while(n >0){
+            int digit = n % 10;
+            sum= sum+digit;
+            n= n/ 10;
+        }
+        System.out.println("Sum of digit of "+org+" is "+sum);
+        sc.close();
+    }
+}
