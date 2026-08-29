26987f242b0f998c788be1f0da94b387b124da16
krishnachaudhary14555 <krishnachaudhary3942@gmail.com>
2026-08-29 13:50:36 +0530

    Reverse Stars Pyramid

---
+++ ReversePyramid.java
@@ -0,0 +1,26 @@
+/*
+
+****
+***
+**
+*
+*/
+
+import java.util.*;
+public class ReversePyramid {
+    public static void main(String[] args){
+Scanner sc= new Scanner(System.in);
+        System.out.println("Enter n :");
+        int n  = sc.nextInt();
+        int stars=n;
+        for(int i=1; i<=n ; i++){
+            for( int j =1; j<=stars; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+            stars--;
+        }
+        sc.close();
+    }
+     
+}
